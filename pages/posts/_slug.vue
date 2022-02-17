d255587fad0777a4031c453ae3e542fee0eb3d18
prazdevs <sachabouillez@gmail.com>
2022-02-17 11:50:10 +0100

    initial commit

---
+++ pages/posts/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ body }}</p>
+    <p>post: {{ id }}</p>
+    <NuxtLink :to="`/posts/${id + 1}`">
+      suivant
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ params, $axios }) {
+    const slug = params.slug
+
+    const { title, body, id } = await $axios.$get(`https://jsonplaceholder.typicode.com/posts/${slug}`)
+
+    return { title, body, id, slug }
+  }
+}
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col v-for="post in posts" :key="post.id" cols="4">
+      <v-card>
+        <v-card-title>{{ post.title }}</v-card-title>
+        <v-card-text>{{ post.body }}</v-card-text>
+        <v-card-actions>
+          <v-btn nuxt :to="`/posts/${post.id}`">
+            Ouvrir
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $axios }) {
+    const posts = await $axios.$get('https://jsonplaceholder.typicode.com/posts')
+
+    return { posts }
+  }
+}
+</script>
