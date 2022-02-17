919d5ba00a5b829ce808134a717b3a9a6d582127
prazdevs <sachabouillez@gmail.com>
2022-02-17 15:18:50 +0100

    add comments to post

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -6,6 +6,16 @@
     <NuxtLink :to="`/posts/${id + 1}`">
       suivant
     </NuxtLink>
+    <v-list>
+      <v-list-item v-for="comment in comments" :key="comment.id" two-line>
+        <v-list-item-content>
+          <v-list-item-title>
+            <span class="text-overline">{{ comment.email }}</span> - {{ comment.name }}
+          </v-list-item-title>
+          <v-list-item-subtitle>{{ comment.body }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
@@ -14,9 +24,12 @@ export default {
   async asyncData ({ params, $axios }) {
     const slug = params.slug
 
-    const { title, body, id } = await $axios.$get(`https://jsonplaceholder.typicode.com/posts/${slug}`)
+    const url = `https://jsonplaceholder.typicode.com/posts/${slug}`
 
-    return { title, body, id, slug }
+    const { title, body, id } = await $axios.$get(url)
+    const comments = await $axios.$get(`${url}/comments`)
+
+    return { title, body, id, comments }
   }
 }
 </script>
